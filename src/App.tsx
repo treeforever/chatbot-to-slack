4e0cbb7c9dbfcdebb7d08771fa6d894d58c64d37
Mengqi Zhu <treeforever7@gmail.com>
2019-11-18 19:44:51 -0500

    move styling to inline

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback, useEffect, CSSProperties } from "react";
 const io = require('socket.io-client');
 const socket = io('http://localhost:8080');
 
@@ -7,13 +7,67 @@ type Message = {
     text: string,
     name: string
 }
-
-const listStyle = (isLeft: boolean) => ({
+const blue = 'rgb(130, 224, 255)';
+const grey = '#f5f6f7';
+const listStyle = (isMe: boolean) => ({
     padding: '5px 10px',
     marginTop: '.5em',
     display: 'flex',
-    justifyContent: isLeft ? 'flex-start' : 'flex-end'
+    justifyContent: isMe ? 'flex-end' : 'flex-start'
 })
+const messageStyle = (isMe: boolean) => ({
+    position: 'relative',
+    borderRadius: '.4em',
+    padding: '1em',
+    background: isMe ? blue : grey,
+    '&:after': {
+        content: '',
+        position: 'absolute',
+        top: '50%',
+        width: 0,
+        height: 0,
+        border: '12px solid transparent',
+        marginTop: '-12px',
+        ...(isMe ? {
+            right: 0,
+            borderLeftColor: blue,
+            borderRight: 0,
+            marginRight: '-12px',
+
+        } : {
+                left: 0,
+                borderRightColor: grey,
+                borderLeft: 0,
+                marginLeft: '-12px',
+
+            })
+    }
+}) as CSSProperties
+const formStyle = {
+    background: '#000',
+    padding: '3px',
+    position: 'fixed',
+    bottom: 0,
+    width: '100%',
+} as CSSProperties
+const inputStyle = {
+    border: 0,
+    padding: '10px',
+    width: '90%',
+    marginRight: '.5%',
+}
+const buttonStyle = {
+    width: '9%',
+    background: blue,
+    border: 'none',
+    padding: '10px',
+}
+const ulStyle = {
+    wordBreak: 'break-word',
+    listStyleType: 'none',
+    margin: 0,
+    padding: 0,
+} as CSSProperties
 
 
 export default () => {
@@ -38,18 +92,24 @@ export default () => {
     }
     return (
         <>
-            <ul id="messages">
+            <ul id="messages" style={ulStyle}>
                 {messages.map((m, index) =>
                     <li
                         key={index + m.text.slice(0, 3)}
                         style={listStyle(m.name === 'me')}
                     >
-                        <span className={m.name === 'me' ? 'my-message' : 'other-message'}>{m.name !== 'me' ? `${m.name}: ` : ''}{m.text}</span>
+                        <span
+                            className={m.name === 'me' ? 'my-message' : 'other-message'}
+                            style={messageStyle(m.name === 'me')}
+                        >
+                            {m.name !== 'me' ? `${m.name}: ` : ''}{m.text}
+                        </span>
                     </li>
                 )}
             </ul>
-            <form action="" onSubmit={onSubmit}>
-                <input id="m" onChange={onChange} value={inputValue} /><button>Send</button>
+            <form action="" onSubmit={onSubmit} style={formStyle}>
+                <input id="m" onChange={onChange} value={inputValue} style={inputStyle} />
+                <button style={buttonStyle}>Send</button>
             </form>
         </>
     )
