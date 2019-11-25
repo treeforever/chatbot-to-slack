007e1e8c9fc762ebb8d4abbf6a3ff1aab3a30af3
Mengqi Zhu <treeforever7@gmail.com>
2019-11-25 10:34:25 -0500

    retrieve conversation

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,17 +6,22 @@ const { RTMClient, LogLevel } = require('@slack/rtm-api');
 require('dotenv').config()
 import { generateRandomChannelName } from './utils';
 
+
 const rtm = new RTMClient(process.env.BOT_USER_TOKEN, {
     logLevel: LogLevel.INFO
 });
+
+const disconnectAnnouncement = '[user disconnected]';
 
 io.on('connection', async (socket: any) => {
     console.log('a user connected');
     let notifyFirstMessage = true;
     let threadTs = '';
 
-    socket.on('retrieve messages by thread ts', (ts: string) => {
+    socket.on('retrieve messages by thread ts', async (ts: string) => {
         threadTs = ts;
+        const chattyLiveChat = 'CQ85K0E0Z'
+        const conversation = await retrieveConversation(chattyLiveChat, threadTs)
     })
 
     socket.on('browser message', async (msg: string) => {
@@ -35,7 +40,7 @@ io.on('connection', async (socket: any) => {
 
     socket.on('disconnect', (reason: string) => {
         if (threadTs && reason === 'transport close') {
-            postMsgToSlackChannel('[user disconnected]', threadTs)
+            postMsgToSlackChannel(disconnectAnnouncement, threadTs)
 
         }
         console.log('Chatbot disconnected', reason)
@@ -53,7 +58,7 @@ io.on('connection', async (socket: any) => {
     }
 
     rtm.on('message', async (event: any) => {
-        console.log('sent from Slack SDK', event);
+        // console.log('sent from Slack SDK', event);
 
         // filter to only replies and sent by users that are not Chatty
         if (event.thread_ts === threadTs && event.username !== 'Chatty') {
@@ -97,6 +102,12 @@ const postMsgToSlackChannel = async (msg: string, threadTs: string): Promise<Tim
     }
 };
 
+const openingMessageStart = 'Someone started a conversation: \n>'
+const slackReturnedOpeningMessageStart = 'Someone started a conversation: \n&gt;'
+const openingMessageEnd = '\nChat starting in the threads'
+const openingMessage = (msg: string) => `${openingMessageStart}${msg}${openingMessageEnd}`
+
+
 /**
  * Notify everyone in #chatty-live-chat that a channel is created to facilitate a new conversation from the website
  * @param msg 
@@ -114,7 +125,7 @@ const initiateChat = async (
         },
         data: {
             channel: 'chatty-live-chat',
-            text: `Someone started a conversation: "${msg}".\nChat starting in the threads`,
+            text: openingMessage(msg),
         },
     });
     try {
@@ -190,9 +201,56 @@ const getUserName = async (user: string) => {
         if (response.data.ok) {
             return response.data.user.profile.real_name_normalized
         } else {
-            console.log('Getting username not ok')
+            console.log('Getting username not ok', response.data.error)
         }
     } catch (err) {
         console.log('Getting username failed', err);
     }
+}
+
+type SlackConversation = Array<{ text: string }>
+
+const retrieveConversation = async (channel: string, ts: string) => {
+    const options: any = {
+        url: `https://slack.com/api/conversations.replies?channel=${channel}&ts=${ts}`,
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/x-www-form-urlencoded',
+            Authorization: `Bearer ${process.env.SLACK_OAUTH_TOKEN}`,
+        },
+    };
+    try {
+        const response = await axios(options);
+        if (response.data.ok) {
+            rebuildConversationForUser(response.data.messages)
+            console.log('getting back conversation')
+        } else {
+            console.log('getting back conversation not ok', response.data.error)
+        }
+        return response.data.messages as SlackConversation;
+    } catch (err) {
+        console.log('Getting back conversation failed', err);
+    }
+}
+type Reply = { text: string, username?: string, user: string }
+const rebuildConversationForUser = async (messages: any) => {
+    const parentMsg = messages[0].text
+    const firstMsg = parentMsg.substring(slackReturnedOpeningMessageStart.length, parentMsg.search(openingMessageEnd))
+    const replyUsers: Array<string> = (messages[0].reply_users as Array<string>).filter((user) => !user.startsWith('B')); // eliminate bot user
+    const getUserNamesMap = async () => {
+        let userNamesMap: { [key: string]: string } = {};
+        await Promise.all(replyUsers.map(async (user) => {
+            userNamesMap[user] = await getUserName(user)
+        }))
+        return userNamesMap;
+    };
+    const userNamesMap: { [key: string]: string } = await getUserNamesMap();
+
+    const replies = messages.slice(1).filter((msg: Reply) => msg.text !== disconnectAnnouncement)
+        .map((msg: Reply) => ({
+            text: msg.text,
+            name: msg.username === 'Chatty' ? 'me' : userNamesMap[msg.user]
+        }))
+
+    return [{ text: firstMsg, name: 'me' }, ...replies];
 }
